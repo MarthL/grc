e0ed878404310a4d0422a1eb07ffd2df50e969ed
martin <martin.aubertin@90tech.fr>
2023-03-08 21:44:50 +0100

    upload dependencies

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-import { Button } from '@mui/material';
+// import { Button } from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const drawerWidth = 240;
