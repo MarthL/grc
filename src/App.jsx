4f35c235194826647bcf08e34086097184c2274a
martin <martin.aubertin@90tech.fr>
2023-03-13 16:12:42 +0100

    error is working, router is working, finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Corpus } from './components/Corpus';
-import Second from './components/Second/Second';
+import Corpus from './components/Corpus/Corpus';
+import Second  from './components/Second/Second';
+import Error from 'components/Error/Error';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Routes, Route } from 'react-router-dom';
 
@@ -20,6 +21,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Corpus />} />
+          <Route path="*" element={<Error />} />
           <Route path="/second" element={<Second />} />
         </Routes>
     </ThemeProvider>
