2d98d4832bac17445a14571fff5c1c6c82bf419a
martin <martin.aubertin@90tech.fr>
2023-03-15 14:35:42 +0100

    adding column populations. very long

--- backend/src/cities/cities.entity.ts
+++ backend/src/cities/cities.entity.ts
@@ -10,4 +10,13 @@ export class Cities extends Model<Cities> {
 
   @Column
   ville_code_postal: number;
+
+  @Column
+  ville_population_1999: number;
+
+  @Column
+  ville_population_2010: number;
+
+  @Column
+  ville_population_2012: number;
 }
