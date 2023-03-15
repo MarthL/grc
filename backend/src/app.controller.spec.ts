d123e56149b24597aa7889962b702e814345dcd2
martin <martin.aubertin@90tech.fr>
2023-03-15 14:05:13 +0100

    changes has been made for testing the backend env using jest. 2/3 tests are actually OK WIP

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseProviders } from './provider/database.provider';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +9,15 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [...databaseProviders, AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World !"', () => {
+      expect(appController.getHello()).toBe('Hello World !');
     });
   });
 });
--- backend/src/cities/cities.service.spec.ts
+++ backend/src/cities/cities.service.spec.ts
@@ -1,13 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CitiesService } from './cities.service';
 import { citiesProviders } from './cities.provider';
+import { CitiesController } from './cities.controller';
 
 describe('CitiesService', () => {
   let service: CitiesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CitiesService],
+      providers: [CitiesService, ...citiesProviders],
+      controllers: [CitiesController],
+      imports: [],
+      exports: [CitiesService],
     }).compile();
 
     service = module.get<CitiesService>(CitiesService);
