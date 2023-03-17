da0a62e6aa5e9b0ea31190149cf5b2649caa9e78
martin <martin.aubertin@90tech.fr>
2023-03-17 16:02:19 +0100

    add id in order to be more easy for redirection get/id

--- src/components/Corpus/Corpus.jsx
+++ src/components/Corpus/Corpus.jsx
@@ -5,26 +5,25 @@ import { React, useState, useEffect } from "react";
 import api from "api/api";
 
 
-const Corpus = (props) => { 
+const Corpus = (props) => {
 
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState({label: '', id: null});
   const [error, setError] = useState(false);
   const [cities, setCities] = useState([]);
   const [loading, setLoading] = useState(true);
   const [selectedOption, setSelectedOption] = useState(null);
 
-
   useEffect(() => {
     setError(false);
-    if(input.length > 2) { 
+    if (input.label.length > 2) {
       setError(false);
-    } else { 
+    } else {
       setError('2 letters minimum');
     }
-    console.log(input)
+    console.log(input.id)
   }, [input])
 
-  useEffect( () => {
+  useEffect(() => {
     api.get('/cities')
       .then((res) => res.data)
       .then((data) => setCities((data)))
@@ -38,56 +37,55 @@ const Corpus = (props) => {
   })
 
   const handleInputChange = (event, newValue) => {
-    setInput(event.target.value);
-    console.log(newValue);
+    setInput({label: event.target.value, id: null});
   };
 
-  return(
+  return (
     <>
-    <Box sx={{width: "auto", textAlign: "center"}}> <h1>Statistiques Population</h1> </Box>
-    <Box sx={{ my: '5vh', mx: 'auto', width: 200 }}>
-    <Autocomplete 
-  filterOptions={filterOptions} 
-  options={cities.cities?.map((value)=> ({
-    id: value.id,
-    label: `${value.ville_nom} ${value.ville_code_postal}`
-  })).sort((a, b) => a.label.localeCompare(b.label)) || [] }
-  value={selectedOption}
-  onChange={(event, newValue) => {
-    setSelectedOption(newValue);
-    setInput(newValue.label);
-  }}
-  renderInput={(params) => (
-    <TextField
-      {...params}
-      label="City"
-      onChange={handleInputChange}
-      value={input}
-      inputProps={{
-        ...params.inputProps,
-        autoComplete: 'off' // disable autocomplete and autocorrect
-      }}
-    />
-  )}
-  getOptionLabel={(option) => option.label}
-/>
-    </Box>
-    <Box display="flex" alignItems="center" justifyContent="center">
-      <Box sm={1} marginRight="10px">
-        <Button color="success" variant="contained">
-          Send
-        </Button>
+      <Box sx={{ width: "auto", textAlign: "center" }}> <h1>Statistiques Population</h1> </Box>
+      <Box sx={{ my: '5vh', mx: 'auto', width: 200 }}>
+        <Autocomplete
+          filterOptions={filterOptions}
+          options={cities.cities?.map((value) => ({
+            id: value.id,
+            label: `${value.ville_nom} ${value.ville_code_postal}`
+          })).sort((a, b) => a.label.localeCompare(b.label)) || []}
+          value={selectedOption}
+          onChange={(event, newValue) => {
+            setSelectedOption(newValue);
+            setInput({label: newValue.label, id: newValue.id})
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="City"
+              onChange={handleInputChange}
+              value={input.label}
+              inputProps={{
+                ...params.inputProps,
+                autoComplete: 'off' // disable autocomplete and autocorrect
+              }}
+            />
+          )}
+          getOptionLabel={(option) => option.label}
+        />
       </Box>
-      <Box sm={1}>
-      <Button color="info" variant="contained"
-      onClick={() => setSelectedOption(null)}
-      >
-        Reset
-      </Button>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Box sm={1} marginRight="10px">
+          <Button color="success" variant="contained">
+            Send
+          </Button>
+        </Box>
+        <Box sm={1}>
+          <Button color="info" variant="contained"
+            onClick={() => setSelectedOption(null)}
+          >
+            Reset
+          </Button>
+        </Box>
       </Box>
-    </Box>
     </>
-)
+  )
 }
 
 export default Corpus;
